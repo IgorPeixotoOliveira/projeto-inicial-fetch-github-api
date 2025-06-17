fe6e44f3b7c079569adeae19ef9905696ed55e36
Igor Peixoto de Oliveira <142852398+IgorPeixotoOliveira@users.noreply.github.com>
2025-06-17 19:52:55 -0300

    index.js ok, criacao do variables.js ok, criação do services/user.js e /repositories.js

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+import { user } from '/src/js/services/user.js'
+import { repositories } from '/src/js/services/repositories.js'
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName);
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode;
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        getUserProfile(userName);
+    }
+})
+
+function getUserProfile(userName) {
+    user(userName).then(userData => {
+        let userInfo = `<div class="info">
+                            <img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
+                            <div class="data">
+                                <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                                <p>${userData.bio ?? 'Não possui bio cadastrado 😢'}</p>
+                                <a href="${userData.html_url}" target="_blank">Ver perfil</a>
+                            </div>
+                        </div>`
+
+        document.querySelector('.profile-data').innerHTML = userInfo
+
+        getUserRepositories(userName)
+    })
+}
+
+function getUserRepositories(userName){
+    repositories(userName).then(reposData => {
+    let repositoriesItens = ""
+
+    reposData.forEach(repo => {
+        repositoriesItens +=    `<li>
+                                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                                </li>`
+    })
+    document.querySelector('.profile-data').innerHTML += 
+    `<div class="repositories section">
+        <h2>Repositórios</h2>
+        <ul class="repositories">${repositoriesItens}</ul>
+    </div>`
+})
+}
